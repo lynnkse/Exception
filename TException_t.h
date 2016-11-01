3832bea87468a9495195935f262422bb333c5623
Anton Gulyaev <lynnkse@gmail.com>
2016-11-01 21:17:53 +0200

    final

--- TException_t.h
+++ TException_t.h
@@ -6,7 +6,7 @@ template <class EXT>
 class TException_t
 {
 	public:
-		TException_t(const string& _desc, const EXT& _data): m_desc(_desc), m_data(_data), m_file(__FILE__), m_line(__LINE__){}
+		TException_t(const string& _desc, const EXT& _data, const string& _fileName, int _line): m_desc(_desc), m_data(_data), m_file(_fileName), m_line(_line){}
 		~TException_t(){}
 		TException_t(const TException_t& _tex): m_desc(_tex.m_desc), m_data(_tex.m_data), m_file(_tex.m_file), m_line(_tex.m_line){}
 		const string& GetDesc() const { return m_desc; }
--- test.cpp
+++ test.cpp
@@ -2,17 +2,15 @@
 #include <cstring>
 #include "TException_t.h"
 
+#define THROW(T, i, msg) do{T num(i); string str(msg); TException_t<T> ex(str, num, __FILE__, __LINE__); throw(ex); } while(0)  
+
 using namespace std;
 
 int main()
 {
-	cout << "some line here" << endl;
 	try
 	{
-		int i = 17;	
-		string str("Just throwing something");	
-		TException_t<int> ex(str, i);
-		throw(ex);
+		THROW(float, 99.9, "Just some message");
 	}
 	catch(TException_t<int>& _tex)
 	{
@@ -20,7 +18,15 @@ int main()
 		cout << _tex.GetDesc() << endl;
 		cout << _tex.GetFile() << endl;
 		cout << _tex.GetLine() << endl;
-	}	
+	}
+	catch(TException_t<float>& _tex)
+	{
+		cout << _tex.GetData() << endl;
+		cout << _tex.GetDesc() << endl;
+		cout << _tex.GetFile() << endl;
+		cout << _tex.GetLine() << endl;
+	}
+		
 
 	return 0;
 }
